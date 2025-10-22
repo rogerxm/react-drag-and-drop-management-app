9c8f33e867974b5bf60bae00faf3d62ec8082969
Roger Montes <rogerxmontes@gmail.com>
2025-10-22 16:19:24 -0600

    Configuracion inicial del Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { HomePage } from "./pages/HomePage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );

ff9304491b5b833bb5dd5ade37ddf4d8aff1ad2b
Roger Montes <rogerxmontes@gmail.com>
2025-10-23 07:53:15 -0600

    Clases de tailwind para los estilos

--- src/components/DraggableUserCard.tsx
+++ src/components/DraggableUserCard.tsx
@@ -21,13 +21,25 @@ export const DraggableUserCard = ({
   const userEmail = user.email;
 
   return (
-    <div ref={setNodeRef}>
-      <img src={profileImage} alt={fullName} {...listeners} {...attributes} />
+    <div
+      className="p-4 my-4 bg-white rounded-lg shadow border border-gray-200 flex gap-2 items-center transition-shadow hover:shadow-lg"
+      ref={setNodeRef}
+    >
+      <img
+        className="w-20 h-20 rounded-full"
+        src={profileImage}
+        alt={fullName}
+        {...listeners}
+        {...attributes}
+      />
 
-      <Link to={`/users/${user.id}`}>
+      <Link
+        className="p-2 rounded-md hover:bg-gray-50"
+        to={`/users/${user.id}`}
+      >
         <div>
-          <h4>{fullName}</h4>
-          <p>{userEmail}</p>
+          <h4 className="font-semibold text-gray-800">{fullName}</h4>
+          <p className="text-sm text-gray-500">{userEmail}</p>
         </div>
       </Link>
     </div>
--- src/components/DroppableUserList.tsx
+++ src/components/DroppableUserList.tsx
@@ -16,11 +16,23 @@ export const DroppableUserList = ({
   const { isOver, setNodeRef } = useDroppable({ id });
 
   return (
-    <div ref={setNodeRef}>
-      <h3>{title}</h3>
+    <div
+      className="bg-white rounded-xl shadow-lg flex flex-col"
+      ref={setNodeRef}
+    >
+      <div className="p-6 border-b border-gray-200">
+        <h2 className="text-2xl font-semibold text-gray-700">{title}</h2>
+      </div>
 
       <UserList users={users} containerId={id} />
-      {users.length === 0 && <p>{isOver ? "Suelta aquí" : "Lista vacía"}</p>}
+
+      {users.length === 0 && (
+        <div>
+          <p className="text-center text-lg font-semibold">
+            {isOver ? "Suelta aquí" : "Sin usuarios seleccionados"}
+          </p>
+        </div>
+      )}
     </div>
   );
 };

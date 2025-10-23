392008665f9aef3b5af91aacc60dbf314781ddb3
Roger Montes <rogerxmontes@gmail.com>
2025-10-22 19:55:57 -0600

    Se agrega tipado y funciones para manejar el loader y guardar los usuarios en el estado global

--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -1,18 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { User } from "../types/user";
 
-const initialState = {
-  value: 0,
+interface UserState {
+  users: User[];
+  loading: boolean;
+  error: string | null;
+}
+
+const initialState: UserState = {
+  users: [],
+  loading: false,
+  error: null,
 };
 
 export const userSlice = createSlice({
   name: "users",
   initialState,
   reducers: {
-    increment: (state) => {
-      state.value += 1;
+    // Manejar el loader
+    setLoading: (state, action: PayloadAction<boolean>) => {
+      state.loading = action.payload;
+    },
+
+    // Guarda los usuarios de la API
+    setInitialUsers: (state, action: PayloadAction<User[]>) => {
+      state.users = action.payload;
+      state.loading = false;
     },
   },
 });
 
-export const { increment } = userSlice.actions;
+export const { setLoading, setInitialUsers } = userSlice.actions;
 export default userSlice.reducer;

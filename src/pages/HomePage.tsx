b984b13c435efa871575c5e2d0f5774d3cb5e4e7
Roger Montes <rogerxmontes@gmail.com>
2025-10-23 07:53:55 -0600

    Spinner para la carga de la API

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,9 +8,11 @@ import {
   setInitialUsers,
   setLoading,
 } from "../store/userSlice";
-import { NewUserForm } from "../components/NewUserForm";
+
 import { DndContext, type DragEndEvent } from "@dnd-kit/core";
 import { DroppableUserList } from "../components/DroppableUserList";
+import { Link } from "react-router";
+import { BounceLoader } from "react-spinners";
 
 export const HomePage = () => {
   const { generalUsers, selectedUsers, loading } = useSelector(
@@ -37,7 +39,14 @@ export const HomePage = () => {
 
   // Se renderiza un loader si está cargando
   if (loading && generalUsers.length === 0 && selectedUsers.length === 0) {
-    return <div>Cargando usuarios desde la API...</div>;
+    return (
+      <div className="flex w-full h-screen items-center justify-center">
+        <p className="text-2xl font-bold">
+          {/* Cargando usuarios desde la API */}
+          <BounceLoader size={100} color="#484dc5" />
+        </p>
+      </div>
+    );
   }
 
   const handleDragEnd = (event: DragEndEvent) => {
@@ -60,10 +69,24 @@ export const HomePage = () => {
 
   return (
     <DndContext onDragEnd={handleDragEnd}>
-      <div>
-        <h3>Lista de Usuarios</h3>
+      <div className="container mx-auto max-w-6xl p-8">
+        <div className="mb-4 text-center">
+          <h1 className="text-4xl font-bold text-gray-800">Panel Operativo</h1>
+          <p className="text-lg text-gray-500">
+            Arrastra a los usuarios para seleccionarlos
+          </p>
+        </div>
 
-        <div>
+        <div className="flex items-center justify-center mb-6">
+          <Link
+            to="/users/new"
+            className="px-5 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700"
+          >
+            Agregar nuevo usuario
+          </Link>
+        </div>
+
+        <div className="flex gap-5 justify-center">
           <DroppableUserList
             id="general-list"
             title="Lista de general de usuarios"
@@ -75,11 +98,6 @@ export const HomePage = () => {
             title="Lista de usuarios seleccionados"
             users={selectedUsers}
           />
-        </div>
-
-        <div>
-          <h4>Añadir Nuevo Usuario</h4>
-          <NewUserForm />
         </div>
       </div>
     </DndContext>

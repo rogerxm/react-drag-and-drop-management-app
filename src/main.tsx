285df4f6d624c1d5302b3d393585824977e5f854
Roger Montes <rogerxmontes@gmail.com>
2025-10-22 23:56:58 -0600

    Se agrega Tailwind para los estilos

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import { HomePage } from "./pages/HomePage.tsx";
 import { UserDetailsPage } from "./pages/UserDetailsPage.tsx";
+import "./App.css";
 
 const router = createBrowserRouter([
   {

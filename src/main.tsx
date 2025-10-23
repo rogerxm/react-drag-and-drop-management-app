0868f0890859386c9f67ef5b52442e6846adb7cd
Roger Montes <rogerxmontes@gmail.com>
2025-10-22 21:38:04 -0600

    Ruta para el detalle de usuario

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,16 @@ import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import { HomePage } from "./pages/HomePage.tsx";
+import { UserDetailsPage } from "./pages/UserDetailsPage.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/users/:id",
+    element: <UserDetailsPage />,
   },
 ]);
 
